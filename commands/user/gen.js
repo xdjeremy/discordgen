1f125305b555856fb43cc657426d90e453beebd7
44615921+xdjeremy@users.noreply.github.com <jemoyfrancisco@gmail.com>
2021-06-05 02:20:59 +0800

    added dm check & changed .gen to .order

--- commands/user/gen.js
+++ commands/user/gen.js
@@ -6,9 +6,9 @@ const {
 module.exports = class MeowCommand extends Command {
     constructor(client) {
         super(client, {
-            name: 'gen',
+            name: 'order',
             group: 'user',
-            memberName: 'gen',
+            memberName: 'order',
             description: 'Claim account.'
         });
     }
@@ -23,11 +23,12 @@ module.exports = class MeowCommand extends Command {
                 return message.reply(res.body.msg);
             }
 
+
             //give account
             message.reply('Check your dm for the information.');
 
             await message.author.send({embed: {
-                    "title": "Account information",
+                    "title": "Pizza Recipe",
                     "color": 1127848,
                     "fields": [
                         {
@@ -41,6 +42,7 @@ module.exports = class MeowCommand extends Command {
                     ]
                 }
             })
+                .catch(() => message.reply("Can't send dm to user! Enable your DMs."));
 
             return ;
         }

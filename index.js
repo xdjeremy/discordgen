e99a73f7bd30a785ba32e9fd83cbbea64c2943c4
44615921+xdjeremy@users.noreply.github.com <jemoyfrancisco@gmail.com>
2021-06-05 02:24:18 +0800

    changed activity indicator

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.registry
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
-    client.user.setActivity('with Pizza');
+    client.user.setActivity('.order command');
 });
 
 client.on('error', console.error);
